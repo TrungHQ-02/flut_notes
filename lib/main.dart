44bd6d1dec5ce0460fd2b8abc3438c08169638af
Hoàng Quốc Trung <103867359+TrungHQ-02@users.noreply.github.com>
2023-09-21 21:36:38 +0700

    step-4

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flut_notes/views/login_view.dart';
 import 'package:flut_notes/views/register_view.dart';
 // import 'package:flut_notes/views/login_view.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
+// import 'dart:developer' as devtools show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,6 +22,7 @@ void main() {
     routes: {
       '/login/': (context) => const LoginView(),
       '/register/': (context) => const RegisterView(),
+      '/notes/': (context) => const NotesView(),
     },
   ));
 }
