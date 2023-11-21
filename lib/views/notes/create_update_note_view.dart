7ffb4da43aa1efc0660cf85daf0eef4ec897a350
Hoàng Quốc Trung <103867359+TrungHQ-02@users.noreply.github.com>
2023-11-21 11:41:10 +0700

    step-21

--- lib/views/notes/create_update_note_view.dart
+++ lib/views/notes/create_update_note_view.dart
@@ -1,8 +1,10 @@
 import 'package:flut_notes/services/auth/auth_service.dart';
+import 'package:flut_notes/utilities/dialogs/cannot_share_empty_note_dialog.dart';
 import 'package:flut_notes/utilities/generics/get_arguments.dart';
 import 'package:flutter/material.dart';
 import 'package:flut_notes/services/cloud/cloud_note.dart';
 import 'package:flut_notes/services/cloud/firebase_cloud_storage.dart';
+import 'package:share_plus/share_plus.dart';
 
 class CreateUpdateNoteView extends StatefulWidget {
   const CreateUpdateNoteView({super.key});
@@ -97,6 +99,19 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('New note'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              final text = _textController.text;
+              if (_note == null || text.isEmpty) {
+                await showCannotShareEmptyNoteDialog(context);
+              } else {
+                Share.share(text);
+              }
+            },
+            icon: const Icon(Icons.share),
+          ),
+        ],
       ),
       body: FutureBuilder(
         future: createOrGetExistingNote(context),
